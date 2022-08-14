150b9eef2892a8a5df9cc271813d114b72557cbc
Camargo1958 <aldrov_camargo@hotmail.com>
2022-08-14 09:34:06 -0300

    Salvando filme nos favoritos

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -35,6 +35,20 @@ function Filme(){
         }
     }, [navigate, id])
 
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeFlix");
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+        const hasFilme = filmesSalvos.some((filmeSalvo)=> filmeSalvo.id===filme.id)
+        if(hasFilme){
+            alert("ESSE FILME JÁ ESTÁ NA LISTA!");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeFlix", JSON.stringify(filmesSalvos));
+        alert("FILME SALVO COM SUCESSO!");
+    }   
+
     if(loading){
         return(
             <div className='filme-info'>
@@ -53,9 +67,9 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className='area-buttons'>
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
-                    <a target='_blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                    <a target='blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
